896c1226875aa43b324b4f81918a191ea58eed50
Pierre BIZIEN <pbizien@e1r10p23.clusters.42paris.fr>
2023-02-07 18:02:22 +0100

    avant gestion situation ou un arg faux mais les autres tournent quand meme

--- ft_utils.c
+++ ft_utils.c
@@ -6,7 +6,7 @@
 /*   By: pbizien <pbizien@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 10:18:33 by pbizien           #+#    #+#             */
-/*   Updated: 2023/02/07 17:36:17 by pbizien          ###   ########.fr       */
+/*   Updated: 2023/02/07 17:54:25 by pbizien          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ int	ft_init(char **av, t_data *data, char**envp, int ac)
 	data->paths = ft_put_bs(data->paths);
 	data->fd_in = open(av[1], O_RDONLY, 00644);
 	data->fd_out = open(av[ac - 1], O_RDWR | O_TRUNC | O_CREAT, 00644);
+	if (data->fd_out == -1)
+		return (1);
 	data->pipefd1[0] = -1;
 	data->pipefd1[1] = -1;
 	data->pipefd2[0] = -1;
